9cb3d8138a07411187912a46876012a35bf66af0
agrasteau <88926501+agrasteau@users.noreply.github.com>
2024-12-17 11:57:02 +0100

    fix vuetify config

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-toolbar-title>
+      <v-icon left>mdi-home</v-icon> IUT de Laval - Informatique
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    
+    <!-- Boutons de navigation -->
+    <v-btn v-for="(button, index) in buttons" :key="index"  text>
+      <v-icon>{{ button.icon }}</v-icon>
+      {{ button.label }}
+    </v-btn>
+    
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router'; // Importation de RouterLink pour la navigation
+
+export default defineComponent({
+  name: 'Header',
+  components: {
+    RouterLink // Déclaration du composant RouterLink
+  },
+  data() {
+    return {
+      buttons: [
+        { label: 'Étudiants', icon: 'mdi-account', link: '/etudiants' },
+        { label: 'Cours', icon: 'mdi-book', link: '/cours' },
+        { label: 'Notes', icon: 'mdi-pencil', link: '/notes' },
+        { label: 'Statistiques', icon: 'mdi-chart-bar', link: '/statistiques' },
+        { label: 'Déconnexion', icon: 'mdi-logout', link: '/deconnexion' }
+      ]
+    };
+  }
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createVuetify } from 'vuetify';
+import { VApp, VAppBar, VToolbarTitle, VSpacer, VBtn, VIcon, VMain, VContainer } from 'vuetify/components'; // Importation des composants
+import 'vuetify/styles';
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components: {
+    VBtn,
+    VApp,
+    VAppBar,
+    VToolbarTitle,
+    VSpacer,
+    VIcon,
+    VMain,
+    VContainer
+  },
+});
+
+const app = createApp(App);
+app.use(vuetify);
+app.mount('#app');
