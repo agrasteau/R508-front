1bd7383b8423b7e0d7f03a1691376cc20229e794
st0dxi <alanpertue@gmail.com>
2024-12-24 10:58:46 +0100

    chore: affichage des grades

--- src/views/Grades.vue
+++ src/views/Grades.vue
@@ -1,28 +1,73 @@
 <template>
   <v-app>
-
-  <v-main>
-    grades
- </v-main>
+    <v-main>
+      <v-container>
+        <v-list>
+          <ListItem :headers="headers" :items="items" :title="title"/>
+        </v-list>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Header from '../components/Header.vue';  // Import the Header component
-import ListItem from '../components/ListItem.vue'; // Import the ListItem component
+import { defineComponent, onMounted, ref } from "vue";
+import ListItem from "../components/ListItem.vue";
+import api from "../plugins/api";
 
 export default defineComponent({
-  name: 'Grades',
+  name: "Grades",
   components: {
-    Header,
-    ListItem
+    ListItem,
+  },
+  setup() {
+    // Variables réactives
+    const title = "notes des etudiants";
+    const search = ref("");
+    const headers = ref(Array<{ title: string; key: string; align?: string; sortable?: boolean }>);
+    const items = ref<
+      Array<{
+        id: number;
+        studentId: number;
+        courseId: number;
+        grade: number;
+        semester: string;
+        academicYear: string;
+        studentFirstName: string;
+        studentLastName: string;
+        courseCode: string;
+        courseName: string;
+      }>
+    >([]);
+    // Requête API pour récupérer les items
+    const fetchItems = async () => {
+      try {
+        // insertion de valeur en brute
+        // const newStudent = ref({id: 1,firstName: "string",lastName: "string",email: "user@example.com",dateOfBirth: "2024-12-24",studentId: "string"});
+        // await api.post('/students', { ...newStudent.value });
+        // const newCourse = ref({id: 1,code: "string",name: "string",credits: 20,description: "string"});
+        // await api.post('/courses', { ...newCourse.value });
+        // const newGrade = ref({ studentId: 1, courseId: 1, grade: 20, semester: "S1", academicYear: "3341-3215" });
+        // await api.post('/grades', { ...newGrade.value });
+        const response = await api.get("/grades");
+        console.info(response.data);
+        items.value = response.data;
+      } catch (error) {
+        console.error("Failed to fetch items:", error);
+      }
+    };
+    // Appeler fetchItems lors du montage du composant
+    onMounted(() => {
+      fetchItems();
+    });
+    return { search, items };
   },
   methods: {
     handleActionClick() {
       // Action triggered when the button in ListItem is clicked
-      alert('Action button clicked!');
-    }
-  }
+      alert("Action button clicked!");
+    },
+  },
+  
 });
 </script>
